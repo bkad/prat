88c61d06b7b681ba89db6d4468996637fb487d5f
Kevin Le <solnovus@gmail.com>
2012-04-17 00:07:43 -0700

    have some fun with the title

--- chat/views/frontend.py
+++ chat/views/frontend.py
@@ -1,10 +1,16 @@
 import pymongo
 from flask import Blueprint, g, render_template
 import time
+from random import shuffle
 
 frontend = Blueprint("frontend", __name__)
 
 @frontend.route('/')
 def index():
   messages = g.events.find().sort("$natural", pymongo.DESCENDING).limit(100)
-  return render_template('index.htmljinja', messages=messages, time=time)
+  # maybe use backchat, flexjaxlot (it lines it up nicely)
+  name_jumble = ["back", "flex", "jax", "chat", "lot"]
+  shuffle(name_jumble)
+  title = "".join(name_jumble)
+  return render_template('index.htmljinja', messages=messages, time=time, name_jumble=name_jumble,
+      title=title)
