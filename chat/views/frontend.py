b3cb655986d94b4c067f2c2f98f8043a55f8a310
Michael Dietz <mdietz@mdietz.org>
2012-05-08 00:38:46 -0500

    Added fragment identifiers for channel selection

--- chat/views/frontend.py
+++ chat/views/frontend.py
@@ -13,6 +13,7 @@ def index():
   name_jumble = ["back", "flex", "jax", "chat", "lot"]
   shuffle(name_jumble)
   title = "".join(name_jumble)
+  channels = ["general", "backlot", "oosl", "ALL CAPS"]
   return render_template('index.htmljinja', messages=messages, time=time, name_jumble=name_jumble,
       title=title, user_name=g.user['name'], avatar_url=g.user["gravatar"], authed=g.authed,
-      render_template=render_template, markdown_renderer=markdown_renderer)
+      channels=channels, render_template=render_template, markdown_renderer=markdown_renderer)
