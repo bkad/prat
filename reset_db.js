20258e6563dc461606a5512f828b394a6973602a
Kevin Le <solnovus@gmail.com>
2012-07-30 14:38:43 -0700

    ensure channel is indexed in events

--- reset_db.js
+++ reset_db.js
@@ -1,4 +1,5 @@
 db.events.drop()
 db.users.drop()
 db.createCollection("events", {capped: true, size: 1e7, autoIndexId: true});
+db.events.ensureIndex({ channel:1 })
 db.createCollection("users", {autoIndexId:true});
