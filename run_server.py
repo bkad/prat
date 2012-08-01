d2e6159b52c4c73f7f70efc806bbe2d749224494
Kevin Le <solnovus@gmail.com>
2012-07-31 23:31:54 -0700

    evict offline anonymous users from redis on app startup

--- run_server.py
+++ run_server.py
@@ -3,7 +3,7 @@ from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
 from chat import create_app
 
-# temporary fix for evicting client orphan ids until GH-42 is resolved
+# TODO(kle): temporary fix for evicting client orphan ids until GH-42 is resolved
 def remove_all_client_ids(app):
   from chat.datastore import redis_db, user_clients_key
   with app.app_context():
@@ -11,10 +11,21 @@ def remove_all_client_ids(app):
     for key in redis_db.keys(clients_key_prefix + "*"):
       redis_db.delete(key)
 
+# TODO(kle): remove once we've gated on authentication
+def remove_all_anonymous_users_from_channels(app):
+  from chat.datastore import redis_db, redis_channel_key
+  with app.app_context():
+    channel_key_prefix = redis_channel_key("")
+    for key in redis_db.keys(channel_key_prefix + "*"):
+      for email, status in redis_db.hgetall(key).iteritems():
+        if "@" not in email and status == "offline":
+          redis_db.hdel(key, email)
+
 @werkzeug.serving.run_with_reloader
 def run_server():
   app = create_app()
   remove_all_client_ids(app)
+  remove_all_anonymous_users_from_channels(app)
   http_server = WSGIServer(('0.0.0.0',5000), app, handler_class=WebSocketHandler)
   http_server.serve_forever()
 
